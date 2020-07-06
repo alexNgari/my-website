97bc9da2418374e484b403b508c554447589cf12
Alex Ngari <alex.ngari03@gmail.com>
2020-07-07 01:01:44 +0300

    Added 'home' and 'skills' routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SummaryComponent } from './summary/summary.component';
+import { SkillsComponent } from './skills/skills.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SummaryComponent },
+  { path: 'skills', component: SkillsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
