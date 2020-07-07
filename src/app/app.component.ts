07627e5dda98ad778ac082b3fd04f4b7838857dd
Alex Ngari <alex.ngari03@gmail.com>
2020-07-07 23:59:12 +0300

    Added hamburger menu for mobile navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  hamburger_open: boolean = false;
+
+  onClickHamburger() {
+    this.hamburger_open = !this.hamburger_open;
+    console.log(this.hamburger_open)
+  }
 }
